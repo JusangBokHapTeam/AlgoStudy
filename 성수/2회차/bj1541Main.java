70efa54fe1c4141e229fb8a1f4e98b76704aecd2
platycodonv <93230885+platycodonv@users.noreply.github.com>
2025-07-16 20:19:07 +0900

    Create bj1541Main.java

---
+++ 성수/2회차/bj1541Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "+-", true);
+
+        ArrayDeque<String> initQueue = new ArrayDeque<>();
+        ArrayDeque<String> processQueue = new ArrayDeque<>();
+
+        while (st.hasMoreTokens()) {
+            initQueue.offer(st.nextToken());
+        }
+  
+        while (!initQueue.isEmpty()) {
+
+            String currToken = initQueue.pollFirst();
+
+            if ("+".equals(currToken)) {
+                //prevInteger + rightInteger
+                //String leftInteger = initQueue.pollFirst() + String rightInteger = processQueue.pollLast();
+                String temp = String.valueOf(Integer.parseInt(initQueue.pollFirst()) + Integer.parseInt(processQueue.pollLast()));
+                processQueue.add(temp);
+
+            } else { //- 또는 숫자
+                processQueue.offerLast(currToken);
+            }
+
+        }
+        
+        int result = Integer.parseInt(processQueue.pollFirst());
+
+        while(!processQueue.isEmpty()) {
+
+            String currToken = processQueue.pollFirst();
+
+            if ("-".equals(currToken)) {
+                String nextToken = processQueue.pollFirst();
+                result -= Integer.parseInt(nextToken);
+            }
+
+        }
+
+        System.out.print(result);
+
+
+    }
+}
